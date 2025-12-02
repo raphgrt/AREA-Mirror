6cf148b73dc8237f5cea45e812abbdd6e302b967
Laurent Gonzalez <laurent.gonzalez@epitech.eu>
2025-12-02 15:58:18 +0100

    feat: nestjs redis setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,20 @@ import { DrizzleModule } from './db/drizzle.module';
 import { AuthModule } from '@thallesp/nestjs-better-auth';
 import { auth } from './auth'; // Your Better Auth instance
 
+// NOTE:
+// To use Redis in your Module you may add the redis transporter to your module
+// when adding it in the imports like so:
+//     ClientsModule.register([
+//       {
+//         name: 'MATH_SERVICE',
+//         transport: Transport.REDIS,
+//         options: {
+//           host: process.env.REDIS_HOST,
+//           port: process.env.REDIS_PORT,
+//         }
+//       },
+//     ]),
+
 @Module({
   imports: [DrizzleModule, AuthModule.forRoot({ auth })],
   controllers: [AppController],
